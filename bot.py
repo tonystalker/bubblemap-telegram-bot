48840f0ed2f3bfb11edb3ffe6062b226c454f456
Ayush Tripathi <707ayushtripathi@gmail.com>
2025-04-12 00:49:19 +0530

    Remove webdriver-manager dependency

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
